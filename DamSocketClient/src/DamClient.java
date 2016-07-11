76494a0b08f14dc9e5450559a353b5689bab39c4
Maurice Garcia <maurice.garcia.2015@gmail.com>
2016-07-11 02:29:14 -0400

    Inital Update

---
+++ DamSocketClient/src/DamClient.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class DamClient {
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+
+		String sServerAddress = "10.1.10.10";
+		int iPort = 5000;
+	
+		int iCount = 0;
+		
+		while (iCount < 10) {
+			
+			Socket socket = new Socket(sServerAddress, iPort);
+
+			/* Send Command to DAM*/
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			bw.write("150");
+			bw.newLine();
+			bw.flush();
+
+			/* Receive DAM Message */
+			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			System.out.println(reader.readLine() + " - Count -> " + iCount);
+			reader.close();
+			bw.close();
+			
+			socket.close();
+			
+			iCount++;
+		}
+
+	}
+
+
+
+}
