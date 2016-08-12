e5aa634f1789e890e31dda3ffd5c99175c8b4144
Maurice Garcia <maurice.garcia.2015@gmail.com>
2016-08-12 13:45:10 -0400

    ok

---
+++ DamCommPIC.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=DamCommPIC.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/DamCommPIC.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=damcommpic.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/damcommpic.x.tar
